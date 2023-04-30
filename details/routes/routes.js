e5e6412a598c84d700279772be7e194f645f8a0a
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2023-04-30 18:51:19 +0100

    Remove comment

--- details/routes/routes.js
+++ details/routes/routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const controller = require('../controllers/controller');
 
-// define the home page route
 router.get('/:barcode', controller.getByBarcode);
 
 module.exports = router;
